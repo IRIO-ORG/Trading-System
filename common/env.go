37c9adf014053b65f5fd918b83854cdb20f8f5ed
Piotr Wieczorek <53907986+wps0@users.noreply.github.com>
2026-01-02 23:47:34 +0100

    common: Add helpers for reading environmental variables

---
+++ common/env.go
@@ -0,0 +1,31 @@
+package common
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func GetEnv[T any](key string, defaultValue T) (T, error) {
+	v, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue, nil
+	}
+
+	var err error
+	var parsed any
+
+	switch any(defaultValue).(type) {
+	case string:
+		return any(v).(T), nil
+	case int:
+		parsed, err = strconv.Atoi(v)
+	default:
+		return defaultValue, fmt.Errorf("unsupported type for env var %s: %T", key, defaultValue)
+	}
+
+	if err != nil {
+		return defaultValue, fmt.Errorf("failed to parse env %s as %T: %w", key, defaultValue, err)
+	}
+	return parsed.(T), err
+}
