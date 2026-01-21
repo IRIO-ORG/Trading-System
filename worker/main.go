9c6e313542e15c684ad3099e4598587482728b10
Piotr Wieczorek <53907986+wps0@users.noreply.github.com>
2026-01-21 22:53:27 +0100

    Add snapshot producer code

--- worker/main.go
+++ worker/main.go
@@ -19,10 +19,12 @@ import (
 
 // TODO remove once config map kafka is setup
 const (
-	defaultRequestsTopic   = "trade-requests"
-	defaultExecutedTopic   = "executed-trades"
-	defaultConsumerGroupID = "worker-group"
-	defaultWorkerMode      = "engine"
+	defaultRequestsTopic          = "trade-requests"
+	defaultExecutedTopic          = "executed-trades"
+	defaultSnapshotsTopic         = "orderbook-snapshots"
+	defaultConsumerGroupID        = "worker-group"
+	defaultWorkerMode             = "engine"
+	maxPendingSnapshotChannelSize = 8
 )
 
 type TopicProducer struct {
@@ -40,13 +42,15 @@ func (tp *TopicProducer) TPSend(key string, msg proto.Message) error {
 
 // TODO once MVP is done, move to internal package
 type WorkerHandler struct {
-	mode     string
-	producer *TopicProducer
+	mode             string
+	producer         *TopicProducer
+	snapshotProducer *TopicProducer
 }
 
 func main() {
 	requestsTopic, _ := common.GetEnv("REQUESTS_TOPIC", defaultRequestsTopic)
 	executedTopic, _ := common.GetEnv("EXECUTED_TRADES_TOPIC", defaultExecutedTopic)
+	snapshotTopic, _ := common.GetEnv("SNAPSHOTS_TOPIC", defaultSnapshotsTopic)
 	groupID, _ := common.GetEnv("WORKER_GROUP_ID", defaultConsumerGroupID)
 	mode, _ := common.GetEnv("WORKER_MODE", defaultWorkerMode)
 
@@ -68,9 +72,21 @@ func main() {
 		}
 	}()
 
+	snapshotProducer, err := kafka.NewProtoProducer()
+	if err != nil {
+		slog.Error("WORKER: Critical error creating snapshot producer", "error", err)
+		os.Exit(1)
+	}
+	defer func() {
+		if err := snapshotProducer.Close(); err != nil {
+			slog.Error("WORKER: Error closing snapshot producer", "error", err)
+		}
+	}()
+
 	handler := &WorkerHandler{
-		mode:     mode,
-		producer: NewTopicProducer(executedTopic, executedTradesProducer),
+		mode:             mode,
+		producer:         NewTopicProducer(executedTopic, executedTradesProducer),
+		snapshotProducer: NewTopicProducer(snapshotTopic, snapshotProducer),
 	}
 
 	err = kafka.RunConsumerGroup(groupID, []string{requestsTopic}, handler)
@@ -83,90 +99,152 @@ func main() {
 func (h *WorkerHandler) Setup(_ sarama.ConsumerGroupSession) error   { return nil }
 func (h *WorkerHandler) Cleanup(_ sarama.ConsumerGroupSession) error { return nil }
 
-func (h *WorkerHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
-	partition := claim.Partition()
-	eng := newEngine()
+type PartitionHandler struct {
+	partitionId      int32
+	eng              *engine
+	worker           *WorkerHandler
+	snapshotRequests <-chan string
+}
 
-	for msg := range claim.Messages() {
-		if msg == nil {
-			continue
-		}
-
-		if h.mode == "dump" {
-			dumpTradeEvent(msg.Value)
-			session.MarkMessage(msg, "")
-			continue
-		}
-
-		req := &pb.TradeEvent{}
-		if err := proto.Unmarshal(msg.Value, req); err != nil {
-			slog.Error("WORKER: Failed to unmarshal trade request",
-				"error", err,
-				"value", msg.Value,
-			)
-			session.MarkMessage(msg, "")
-			continue
-		}
-
-		slog.Info("Processing Order",
-			"requestId", req.RequestId,
-			"symbol", req.Trade.Instrument.Symbol,
-			"side", req.Trade.Side,
-			"price", req.Trade.Price,
-			"size", req.Trade.Size,
-		)
-
-		reqID := req.GetRequestId()
-		symbol := req.GetTrade().GetInstrument().GetSymbol()
-
-		execs, err := eng.onTrade(req)
-		if err != nil {
-			slog.Warn("WORKER: onTrade failed",
-				"error", err,
-				"request_id", reqID,
-				"symbol", symbol,
-				"partition", partition,
-				"offset", msg.Offset,
-			)
-			session.MarkMessage(msg, "")
-			continue
-		}
-
-		if len(execs) == 0 {
-			slog.Info("WORKER: accepted (no match)",
-				"request_id", reqID,
-				"symbol", symbol,
-				"partition", partition,
-				"offset", msg.Offset,
-			)
-			session.MarkMessage(msg, "")
-			continue
-		}
-
-		for _, ex := range execs {
-			executedTrade := &pb.ExecutedTradeEvent{
-				Symbol:       ex.symbol,
-				Price:        ex.price,
-				Size:         ex.size,
-				ExecutionId:  newExecutedID(),
-				BidRequestId: ex.buyID,
-				AskRequestId: ex.sellID,
-				ExecutedAt:   timestamppb.New(ex.execTime),
-			}
-			if err := h.producer.TPSend(ex.symbol, executedTrade); err != nil {
-				slog.Error("WORKER: failed to send executed trade",
-					"error", err,
-					"executed-trade", executedTrade,
-				)
-				return err
-			}
-
-			slog.Info("WORKER: executed", "executed-trade", executedTrade.String())
-		}
-
-		session.MarkMessage(msg, "")
+func (h *PartitionHandler) processMessage(msg *sarama.ConsumerMessage, session sarama.ConsumerGroupSession) error {
+	if msg == nil {
+		return nil
 	}
+
+	if h.worker.mode == "dump" {
+		dumpTradeEvent(msg.Value)
+		session.MarkMessage(msg, "")
+		return nil
+	}
+
+	req := &pb.TradeEvent{}
+	if err := proto.Unmarshal(msg.Value, req); err != nil {
+		slog.Error("WORKER: Failed to unmarshal trade request",
+			"error", err,
+			"value", msg.Value,
+		)
+		session.MarkMessage(msg, "")
+		return nil
+	}
+
+	slog.Info("Processing Order",
+		"requestId", req.RequestId,
+		"symbol", req.Trade.Instrument.Symbol,
+		"side", req.Trade.Side,
+		"price", req.Trade.Price,
+		"size", req.Trade.Size,
+	)
+
+	reqID := req.GetRequestId()
+	symbol := req.GetTrade().GetInstrument().GetSymbol()
+
+	execs, err := h.eng.onTrade(req)
+	if err != nil {
+		slog.Warn("WORKER: onTrade failed",
+			"error", err,
+			"request_id", reqID,
+			"symbol", symbol,
+			"partition", h.partitionId,
+			"offset", msg.Offset,
+		)
+		session.MarkMessage(msg, "")
+		return nil
+	}
+
+	if len(execs) == 0 {
+		slog.Info("WORKER: accepted (no match)",
+			"request_id", reqID,
+			"symbol", symbol,
+			"partition", h.partitionId,
+			"offset", msg.Offset,
+		)
+		session.MarkMessage(msg, "")
+		return nil
+	}
+
+	for _, ex := range execs {
+		executedTrade := &pb.ExecutedTradeEvent{
+			Symbol:       ex.symbol,
+			Price:        ex.price,
+			Size:         ex.size,
+			ExecutionId:  newExecutedID(),
+			BidRequestId: ex.buyID,
+			AskRequestId: ex.sellID,
+			ExecutedAt:   timestamppb.New(ex.execTime),
+		}
+		if err := h.worker.producer.TPSend(ex.symbol, executedTrade); err != nil {
+			slog.Error("WORKER: failed to send executed trade",
+				"error", err,
+				"executed-trade", executedTrade,
+			)
+			return err
+		}
+
+		slog.Info("WORKER: executed", "executed-trade", executedTrade.String())
+	}
+
+	session.MarkMessage(msg, "")
 	return nil
+}
+
+func (h *PartitionHandler) processSnapshot(symbol string) error {
+	book, exists := h.eng.books[symbol]
+	if !exists {
+		slog.Warn("Snapshot request for unknown symbol", "symbol", symbol)
+		return nil
+	}
+
+	orders := make([]pb.Order, 1)
+	for _, ask := range book.asks {
+		orders = append(orders, pb.Order{
+			RequestId: ask.id,
+			Side:      ask.side,
+			Price:     ask.price,
+			Size:      ask.remaining,
+			Seq:       ask.seq,
+		})
+	}
+	for _, bid := range book.bids {
+		orders = append(orders, pb.Order{
+			RequestId: bid.id,
+			Side:      bid.side,
+			Price:     bid.price,
+			Size:      bid.remaining,
+			Seq:       bid.seq,
+		})
+	}
+
+	return h.worker.snapshotProducer.TPSend(symbol, &pb.OrderBookSnapshot{
+		CreatedAt: timestamppb.Now(),
+		Symbol:    symbol,
+		Orders:    orders,
+	})
+}
+
+func (h *WorkerHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	partitionHandler := PartitionHandler{
+		partitionId:      claim.Partition(),
+		eng:              newEngine(),
+		worker:           h,
+		snapshotRequests: make(<-chan string, maxPendingSnapshotChannelSize),
+	}
+
+	for {
+		select {
+		case msg, ok := <-claim.Messages():
+			if !ok {
+				// Channel closed
+				return nil
+			}
+			partitionHandler.processMessage(msg, session)
+		case symbol, ok := <-partitionHandler.snapshotRequests:
+			if !ok {
+				// This shouldn't happen
+				return fmt.Errorf("Snapshot channel closed unexpectedly!")
+			}
+			partitionHandler.processSnapshot(symbol)
+		}
+	}
 }
 
 func newExecutedID() string {
