f6b006569632fc2f24f0e3defb0ad581da2c3e91
Tomasz Głąb <32012724+Toomimi@users.noreply.github.com>
2026-01-23 00:38:19 +0100

    Fix watching db script (#54)

--- scripts/watch_executed_trades.sh
+++ scripts/watch_executed_trades.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+
 while true
 do
+  OUTPUT=$(kubectl exec my-postgres-postgresql-0 -- env PGPASSWORD=postgres psql -U postgres -d trading_db -c "SELECT executed_at, symbol, price, size FROM trades ORDER BY executed_at DESC LIMIT 20;" 2>&1)
+
   clear
   echo "--- Last 20 transactions (refreshed every 1s) ---"
-
-  kubectl exec my-postgres-postgresql-0 -- psql -U postgres -d trading_db -c "SELECT executed_at, symbol, price, size FROM trades ORDER BY executed_at DESC LIMIT 20;"
+  echo "$OUTPUT"
 
   sleep 1
 done
